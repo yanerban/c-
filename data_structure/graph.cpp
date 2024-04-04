0e4b818376ffef7ac68273f50a8861ac410aa891
wuanqi <619746351@qq.com>
2024-04-05 00:03:44 +0800

    Check clangd

---
+++ data_structure/graph.cpp
@@ -0,0 +1,21 @@
+
+#include <cstdio>
+#include <vector>
+
+class A {
+
+public:
+
+    int a_;
+    int b_;
+    int c_;
+    int d_;
+    
+};
+int main() {
+
+    printf("hello world\n");
+    std::vector<int> v;
+    
+    return 0;
+}
